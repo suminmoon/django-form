3e9fa3cd69099db044518f7c2d27b7844bc97e9f
edu-jason <kimsj1015@aol.com>
2019-06-17 14:42:12 +0900

    Implement Board delete logic

--- boards/forms.py
+++ boards/forms.py
@@ -2,5 +2,24 @@ from django import forms
 
 
 class BoardForm(forms.Form):
-    title = forms.CharField()
-    content = forms.CharField()
+    title = forms.CharField(
+        max_length=20,
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'title',
+                'placeholder': 'Enter the title',
+            }
+        )
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'rows': 5,
+                'cols': 50,
+                'placeholder': 'Enter the content',
+                'class': 'content-type',
+            }
+        )
+    )
--- boards/views.py
+++ boards/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST, require_GET, require_http_methods
 from .models import Board
 from .forms import BoardForm
 
 
 # Board 의 리스트
+@require_GET
 def index(request):
     boards = Board.objects.order_by('-pk')
     context = {'boards': boards}
     return render(request, 'boards/index.html', context)
 
 
+@require_http_methods(['GET', 'POST'])
 def create(request):
     if request.method == 'POST':
         form = BoardForm(request.POST)
@@ -25,7 +28,17 @@ def create(request):
 
 
 # boards/3/
+@require_GET
 def detail(request, board_pk):
     board = get_object_or_404(Board, pk=board_pk)
     context = {'board': board}
     return render(request, 'boards/detail.html', context)
+
+
+# POST boards/3/delete/
+@require_POST
+def delete(request, board_pk):
+    # 특정 보드를 불러와서 삭제한다.
+    board = get_object_or_404(Board, pk=board_pk)
+    board.delete()
+    return redirect('boards:index')
