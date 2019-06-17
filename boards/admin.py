1f5969069baf53b6fa3154f64bcbad7c4b95f74b
edu-jason <kimsj1015@aol.com>
2019-06-17 10:19:59 +0900

    Create BoardAdmin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Board
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at', 'updated_at', )
+    readonly_fields = ['created_at', 'updated_at', ]
