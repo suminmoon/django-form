5d35aaa8f7ad0dfd9b15259c5b62523becc2beb3
edu-jason <kimsj1015@aol.com>
2019-06-17 10:49:08 +0900

    Create Board index page

---
+++ boards/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# 경로 앱 이름 지정 'boards:detail'
+app_name = 'boards'
+
+# boards/
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Board
 
-# Create your views here.
+
+# Board 의 리스트
+def index(request):
+    boards = Board.objects.order_by('-pk')
+    context = {'boards': boards}
+    return render(request, 'boards/index.html', context)
